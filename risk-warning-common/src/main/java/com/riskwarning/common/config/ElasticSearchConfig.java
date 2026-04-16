28ca586ea5ee108b9c81c8307028236f980f2ab3
221250168 <221250168@smail.nju.edu.cn>
2026-04-16 13:10:33 +0800

    feat: add Elasticsearch authentication support in configuration

--- risk-warning-common/src/main/java/com/riskwarning/common/config/ElasticSearchConfig.java
+++ risk-warning-common/src/main/java/com/riskwarning/common/config/ElasticSearchConfig.java
@@ -12,8 +12,13 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +28,12 @@ public class ElasticSearchConfig {
 
     @Value("${spring.elasticsearch.uris}")
     private String esUris;
+
+    @Value("${spring.elasticsearch.username:}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password:}")
+    private String password;
 
     public static final String INDICATOR_INDEX = "t_indicator";
 
@@ -34,7 +45,17 @@ public class ElasticSearchConfig {
 
     @Bean
     public ElasticsearchClient elasticsearchClient() {
-        RestClient restClient = RestClient.builder(HttpHost.create(esUris)).build();
+        RestClientBuilder builder = RestClient.builder(HttpHost.create(esUris));
+
+        if (username != null && !username.isEmpty()) {
+            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+            builder.setHttpClientConfigCallback(httpClientBuilder ->
+                    httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+            );
+        }
+
+        RestClient restClient = builder.build();
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
